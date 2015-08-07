cc8ce6bbfc7d01c3d2b66b651fbb6ed7e8f31c85
Phanindra Anchuri <phani.ufl@gmail.com>
2015-08-07 14:16:58 -0600

    Added dependency on beautiful soup.

--- src/crawler.py
+++ src/crawler.py
@@ -1,6 +1,11 @@
 __author__ = 'panchuri'
 
+from bs4 import BeautifulSoup
+import urllib2
 
 class Hello:
     def sayHello(self):
-        return 'Hello World'
+        url = 'http://www.google.com'
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page.read())
+        return soup.title
--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -5,10 +5,10 @@ import unittest
 from src.crawler import Hello
 
 
-class MyTestCase(unittest.TestCase):
+class CrawlerTest(unittest.TestCase):
     def hello_test(self):
         h = Hello()
-        self.assertEqual('Hello World', h.sayHello())
+        self.assertTrue('Google' in h.sayHello())
 
 
 if __name__ == '__main__':
