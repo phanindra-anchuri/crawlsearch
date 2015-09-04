705a4bf2d46a942c7222832f02179c636006d991
Chris Loverchio <chrisloverchio@gmail.com>
2015-09-03 21:10:59 -0600

    Added functionality for depth based crawling.

--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from urllib2 import URLError
 import unittest
 
 from src.crawler import Crawler
@@ -6,13 +7,21 @@ from src.crawler import Crawler
 
 class CrawlerTest(unittest.TestCase):
 
-    def crawl_test(self):
-        h = Crawler('http://www.google.com')
-        self.assertTrue(len(h.extract_links()) > 0)
+    def extract_test(self):
+        url = 'http://www.google.com'
+        h = Crawler(url, 1)
+        self.assertTrue(len(h.extract_links(url)) > 0)
 
     def robots_test(self):
-        h = Crawler('http://www.google.com')
-        self.assertTrue('path/catalogs' or 'http://www.google.com/catalogs' not in h.extract_links())
+        url = 'http://www.google.com'
+        h = Crawler(url, 1)
+        self.assertTrue('path/catalogs' and 'http://www.google.com/catalogs' not in h.extract_links(url))
+
+    def crawl_test(self):
+        url = 'invalidurl:'
+        h = Crawler(url, 1)
+        self.assertIsNone(h.crawl_links())
+        self.assertRaises(URLError, h.crawl_links())
 
 if __name__ == '__main__':
     unittest.main()
